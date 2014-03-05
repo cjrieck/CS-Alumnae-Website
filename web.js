08806221d180789f6eee31e1dfc055c147992053
csadmin <csadmin@Leopold.(none)>
2014-03-05 16:37:45 -0500

    uhhhh

--- web.js
+++ web.js
@@ -4,16 +4,8 @@ var express = require('express');
  	logfmt = require('logfmt');
 	mongodb = require('mongodb');
  	MongoClient = mongodb.MongoClient;
- 	linkedin_client = require('linkedin-js')('77lw834nbnef0f', '2MqLprpXwJelFpQn', 'http://facsem.herokuapp.com/');
- 	gm=require('googlemaps');
- 	util=require('util');
 
 
- 	gm.reverseGeocode('41.850033,-87.6500523', function(err, data){
-  util.puts(JSON.stringify(data));
-});
-
- 	
 var app = express();
 var MONGO_URL=process.env.MONGOHQ_URL;
 
@@ -63,21 +55,3 @@ var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
-	function onLinkedInAuth() {
-		IN.API.Profile("me").result( function(me) {
-			var id=me.values[0].id;
-			console.log(me.values[0].lastName);
-		});
-	}
-
-	function initialize() {
-        var mapOptions = {
-          center: new google.maps.LatLng(41.96727,-71.18495),
-          zoom: 3
-        };
-        var map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-      }
-      google.maps.event.addDomListener(window, 'load', initialize);

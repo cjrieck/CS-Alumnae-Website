804ab8f5ad85354e4c5e72f79cc501e7b834e25d
Clayton Rieck <cjrieck123@gmail.com>
2014-04-01 12:48:12 -0400

    fixed

--- web.js
+++ web.js
@@ -129,12 +129,12 @@ app.get('/map-pins', function(req, res){
 
 		collection.find({}).toArray(function(err, items) {
 			// console.log(items);
-			// res.json(items);
-			context = {people: items};
-			console.log("CONTEXT: "+items);
+			res.json(items);
+			// context = {people: items};
+			// console.log("CONTEXT: "+items);
 
 
-			res.render('home', context ); // first page to load
+			// res.render('home', context ); // first page to load
 		});
 
 	});

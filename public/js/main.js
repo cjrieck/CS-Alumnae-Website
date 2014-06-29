572fae1a45f5e44c3c509797d21c1632eed489b5
Clayton Rieck <cjrieck123@gmail.com>
2014-06-29 11:28:04 -0400

    added ripple animation on click

--- public/js/main.js
+++ public/js/main.js
@@ -353,35 +353,45 @@ $(function() {
 	    });
   	}
   
-    $(".item").click(function(e){
-      var $clicked = $(this);
-      
-      //gets the clicked coordinates
-      var offset = $clicked.offset();
-	  var relativeX = (e.pageX - offset.left);
-	  var relativeY = (e.pageY - offset.top);
-      var width = $clicked.width();
+    $(document).on("click", ".item", function(e){
+		e.preventDefault();
+		var $clicked = $(this);
+		var href;
+		if ($clicked.parent(".profile-link").length > 0) {
+			href = $clicked.parent(".profile-link").href;
+		};
+		
+		//gets the clicked coordinates
+		var offset = $clicked.offset();
+		var relativeX = (e.pageX - offset.left);
+		var relativeY = (e.pageY - offset.top);
+		var width = $clicked.width();
 
-      var $ripple = $clicked.find('.ripple');
-      
-      //puts the ripple in the clicked coordinates without animation
-      $ripple.addClass("notransition");
-      $ripple.css({"top" : relativeY, "left": relativeX});
-      $ripple[0].offsetHeight;
-      $ripple.removeClass("notransition");
-      
-      //animates the button and the ripple
-      //$clicked.addClass("hovered");
-      $ripple.css({ "width": width * 2, "height": width*2, "margin-left": -width, "margin-top": -width });
-      
-      setTimeout(function(){
-          //resets the overlay and button
-          $ripple.addClass("notransition");
-          $ripple.attr("style", "");
-          $ripple[0].offsetHeight;
-          //$clicked.removeClass("hovered");
-        $ripple.removeClass("notransition");
-      }, 300 );
+		var $ripple = $clicked.find('.ripple');
+
+		//puts the ripple in the clicked coordinates without animation
+		$ripple.addClass("notransition");
+		$ripple.css({"top" : relativeY, "left": relativeX});
+		$ripple[0].offsetHeight;
+		$ripple.removeClass("notransition");
+
+		//animates the button and the ripple
+		//$clicked.addClass("hovered");
+		$ripple.css({ "width": width * 2, "height": width*2, "margin-left": -width, "margin-top": -width });
+
+		setTimeout(function(){
+			//resets the overlay and button
+			$ripple.addClass("notransition");
+			$ripple.attr("style", "");
+			$ripple[0].offsetHeight;
+			//$clicked.removeClass("hovered");
+			$ripple.removeClass("notransition");
+			
+			if (href) {
+				window.location = href;
+			};
+
+		}, 300 );
     });
 
 	function initialize() {
